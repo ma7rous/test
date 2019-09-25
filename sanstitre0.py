7a4ff62122cc629a9ecdb05256e49cd99d1a8fbb
OUSSAMA ZIDI <ma7rous@users.noreply.github.com>
2019-09-25 15:23:18 +0200

    Update sanstitre0.py

--- sanstitre0.py
+++ sanstitre0.py
@@ -1,17 +1,21 @@
 class Point:
-    
-    i = 12345
-
-    def __init__(self):
+    def __init__(self,x=0,y=0):
         #print('initialiser',self)
-        self.x=0
-        self.y=0
+        self.x = x
+        self.y = y
         
-        def __str__(self):
-             return f'({self.x},{self.y})'
-p = Point()
+    def __str__(self):
+        return f'({self.x},{self.y})'
+    
+    def __add__(self,r):
+        return Point(self.x+r.x,self.y+r.y)    
+    
+p = Point(10,20)
+q = Point(30,40)
+def AddPoint(l,r):
+    return Point(l.x+r.x, l.y+r.y)
 #print(p.x,p.y)
-print(p,str(p))
+#print(p,str(p))
 
 
          
